1ed9c2fb02d1f72c0b2b1290438db53ba137c530
zih_wei <a0917241391@gmail.com>
2025-03-14 16:16:52 +0800

    first commit

---
+++ src/hw1/hw1.java
@@ -0,0 +1,39 @@
+package hw1;
+
+public class hw1 {
+
+	/*
+	• 請設計一隻Java程式,計算12,6這兩個數值的和與積
+	• 請設計一隻Java程式,計算200顆蛋共是幾打幾顆? (一打為12顆)
+	• 請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+	• 請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積與圓周長
+	• 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本
+	金加利息共有多少錢 (用複利計算,公式請自行google)
+	• 請寫一隻程式,利用System.out.println()印出以下三個運算式結果:
+	5 + 5
+	5 + ‘5’
+	5 + “5”
+	並請用註解各別說明答案的產生原因	
+	*/
+	public static void main(String[] args) {
+	
+	int num1 = 12, num2 = 6;
+	System.out.println(num1 + num2);
+	System.out.println(num1 * num2);
+	System.out.println("===============");
+	
+	int num3 = 200, num4 = 12;
+	System.out.println(num3 / num4 );
+	System.out.println(num3 % num4 );
+	System.out.println("===============");
+	
+	int num5 = 256559;
+	int days = num5/(60*60*24);
+	num5 = num5%(60*60*24);
+	int hours = 
+	
+	System.out.println( + "");
+	System.out.println("===============");
+	
+	}
+}
---
+++ src/hw2/hw2_2.java
@@ -0,0 +1,14 @@
+package hw2;
+//• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
+public class hw2_2 {
+
+	public static void main(String[] args) {
+	
+		int i, sum = 0;
+		
+		for ( i = 1; i <= 10; i++ ) {
+			sum = sum * i ;
+		}
+		System.out.println
+	}
+}
